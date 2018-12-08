f0baec99e821610249c5dbb66a42b4e024073ed7
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-12-08 00:28:02 -0300

    Add tank movement, using AddForceAtLocation in the tank tracks

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -1,7 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankTrack.h"
+#include "Engine/StaticMesh.h"
 
+void UTankTrack::SetThrottle(float Throttle){
+	UE_LOG(LogTemp, Warning, TEXT("Throttle: %f"), Throttle)
 
+	FVector ForceApplied = GetForwardVector() * Throttle * MaxTrackDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	UPrimitiveComponent * TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
 
 
