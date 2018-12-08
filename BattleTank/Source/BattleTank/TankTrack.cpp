c9fd1520a0caf1c4b3a911e37a803d5f1940dbde
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-12-08 11:53:25 -0300

    Remove LOG from TankTrack

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -4,8 +4,6 @@
 #include "Engine/StaticMesh.h"
 
 void UTankTrack::SetThrottle(float Throttle){
-	UE_LOG(LogTemp, Warning, TEXT("Throttle: %f"), Throttle)
-
 	FVector ForceApplied = GetForwardVector() * Throttle * MaxTrackDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
 	UPrimitiveComponent * TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
